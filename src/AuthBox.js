f60aeaa12cd13d266a0dad393f6dfb2586f82e9c
baurine <2008.hbl@gmail.com>
2018-06-15 12:13:59 +0800

    login

--- src/AuthBox.js
+++ src/AuthBox.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import { firebaseAuth } from './firebase'
 
 export default class AuthBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      message: ''
+    }
+  }
+
+  inputChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    this.setState({
+      [name]: value
+    })
+    console.log({
+      [name]: value
+    })
+  }
+
+  login = () => {
+    const { email, password } = this.state
+    firebaseAuth.signInWithEmailAndPassword(email, password)
+      .then(user => this.setState({message: JSON.stringify(user)}))
+      .catch(err => this.setState({message: JSON.stringify(err)}))
+  }
+
   render() {
     return (
       <div>
-        <span>Email:</span>
-        <input type='text' name='email'/>
-        <span>Password</span>
-        <input type='password' name='password'/>
-        <button>Login</button>
-        <button>Signup</button>
+        <div>
+          <span>Email:</span>
+          <input type='text' name='email' onChange={this.inputChange}/>
+          <span>Password</span>
+          <input type='password' name='password' onChange={this.inputChange}/>
+          <button onClick={this.login}>Login</button>
+          <button onClick={this.signup}>Signup</button>
+        </div>
+        <p>{this.state.message}</p>
       </div>
     )
   }
